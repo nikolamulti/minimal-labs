74ea9243319f6631948dbc7a8349934a091ceaef
Nikola Radovanovic <multisprojects@gmail.com>
2024-09-30 08:16:42 +0200

    Fixed issue with Home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">This is the home page.</p>
-    </main>
-  );
+  redirect("/svelte");
 }
