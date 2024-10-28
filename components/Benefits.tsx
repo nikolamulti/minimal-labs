2ad85fba9b68153586194b8531703928d7191938
Nikola Radovanovic <multisprojects@gmail.com>
2024-10-28 17:55:18 +0100

    Benefits fix

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "React";
+import React from "react";
 import { Section } from "@/components/ui/Section";
 import Image from "next/image";
 import { Card } from "@/components/ui/Card";
