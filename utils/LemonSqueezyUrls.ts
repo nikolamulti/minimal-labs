011191890008a1d2a5b162f9c9af3ddad02064f6
Nikola Radovanovic <multisprojects@gmail.com>
2024-10-28 19:10:05 +0100

    Completed - Live

--- utils/LemonSqueezyUrls.ts
+++ utils/LemonSqueezyUrls.ts
@@ -1,6 +1,6 @@
 const personalCheckoutUrl =
-  "https://minimal-square.lemonsqueezy.com/buy/1eae4251-b3f3-4f7c-b676-cf42eff96816?embed=1";
+  "https://minimal-square.lemonsqueezy.com/buy/8f9780e9-e050-4222-ab2e-fa892bb683fa?embed=1";
 const teamCheckoutUrl =
-  "https://minimal-square.lemonsqueezy.com/buy/9b54582e-5dae-4785-bb20-05d9fecc3aad?embed=1";
+  "https://minimal-square.lemonsqueezy.com/buy/572b778a-4cc4-46a9-afc3-8b280d7e5eb0?embed=1";
 
 export { personalCheckoutUrl, teamCheckoutUrl };
