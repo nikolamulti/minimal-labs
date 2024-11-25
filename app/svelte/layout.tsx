55759afdd6c384725bf6356e43d49347d6a59c88
Aleksandar Radovanovic <multisprojects@gmail.com>
2024-11-25 23:55:21 +0100

    Google Tag Manager added

--- app/svelte/layout.tsx
+++ app/svelte/layout.tsx
@@ -8,6 +8,37 @@ export default function Layout({
   children: ReactNode;
 }): React.ReactElement {
   return (
-    <div className="selection:bg-white selection:fg-inverted">{children}</div>
+    <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KF4LP92J');`,
+          }}
+        />
+        <title>Your Site Title</title>
+        <meta name="description" content="Your site description" />
+        {/* Add other head elements as needed */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KF4LP92J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <div className="selection:bg-white selection:fg-inverted">
+          {children}
+        </div>
+      </body>
+    </html>
   );
 }
