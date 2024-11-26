018a521c26a87e727ce30e4ee49676f5fb408810
Nikola Radovanovic <multisprojects@gmail.com>
2024-11-26 23:26:35 +0100

    fix: scripts (lemon & analytics)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import type { Metadata } from "next/types";
-import type { Viewport } from "next/types";
+import { Metadata } from "next";
+import type { Viewport } from "next";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -75,6 +76,20 @@ export const viewport: Viewport = {
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <Script
+        src="https://app.lemonsqueezy.com/js/lemon.js"
+        strategy="afterInteractive"
+        defer
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KF4LP92J');`,
+        }}
+      />
       <link
         rel="icon"
         type="image/png"
@@ -92,6 +107,14 @@ export default function Layout({ children }: { children: ReactNode }) {
       <link rel="manifest" href="/app/site.webmanifest" />
       <GoogleTagManager gtmId="GTM-KF4LP92J" />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KF4LP92J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
