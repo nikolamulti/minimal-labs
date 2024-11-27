bd9dd9b292126302fc360b0fac615f581e862ae5
Nikola Radovanovic <multisprojects@gmail.com>
2024-11-27 20:45:51 +0100

    init v2

---
+++ components/layout/root-toggle.tsx
@@ -0,0 +1,105 @@
+'use client';
+import { ChevronDown } from 'lucide-react';
+import { type HTMLAttributes, type ReactNode, useMemo, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '../../lib/cn';
+import { isActive } from '../../lib/is-active';
+import { useSidebar } from 'fumadocs-ui/provider';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import type { PageTree } from 'fumadocs-core/server';
+import { useTreePath } from 'fumadocs-ui/provider';
+
+export interface Option {
+  /**
+   * Redirect URL of the folder, usually the index page
+   */
+  url: string;
+
+  icon?: ReactNode;
+  title: ReactNode;
+  description?: ReactNode;
+
+  /**
+   * Detect from page tree nodes
+   */
+  folder?: PageTree.Folder;
+
+  props?: HTMLAttributes<HTMLElement>;
+}
+
+export function RootToggle({
+  options,
+  ...props
+}: {
+  options: Option[];
+} & HTMLAttributes<HTMLButtonElement>) {
+  const [open, setOpen] = useState(false);
+  const { closeOnRedirect } = useSidebar();
+  const pathname = usePathname();
+  const path = useTreePath();
+
+  const selected = useMemo(() => {
+    return options.findLast((item) =>
+      item.folder
+        ? path.includes(item.folder)
+        : isActive(item.url, pathname, true),
+    );
+  }, [path, options, pathname]);
+
+  const onClick = () => {
+    closeOnRedirect.current = false;
+    setOpen(false);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        {...props}
+        className={cn(
+          'flex flex-row items-center gap-2 rounded-lg px-2 py-1.5 hover:bg-fd-accent/50 hover:text-fd-accent-foreground',
+          props.className,
+        )}
+      >
+        {selected ? <Item {...selected} /> : null}
+
+        <ChevronDown className="me-1.5 size-4 text-fd-muted-foreground" />
+      </PopoverTrigger>
+      <PopoverContent className="w-[var(--radix-popover-trigger-width)] overflow-hidden p-0">
+        {options.map((item) => (
+          <Link
+            key={item.url}
+            href={item.url}
+            onClick={onClick}
+            {...item.props}
+            className={cn(
+              'flex w-full flex-row items-center gap-2 px-2 py-1.5',
+              selected === item
+                ? 'bg-fd-accent text-fd-accent-foreground'
+                : 'hover:bg-fd-accent/50',
+              item.props?.className,
+            )}
+          >
+            <Item {...item} />
+          </Link>
+        ))}
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function Item(props: Option) {
+  return (
+    <>
+      {props.icon}
+      <div className="flex-1 text-start">
+        <p className="text-sm font-medium">{props.title}</p>
+        {props.description ? (
+          <p className="text-xs text-fd-muted-foreground">
+            {props.description}
+          </p>
+        ) : null}
+      </div>
+    </>
+  );
+}
---
+++ lib/get-sidebar-tabs.tsx
@@ -0,0 +1,37 @@
+import type { PageTree } from 'fumadocs-core/server';
+import type { Option } from '../components/layout/root-toggle';
+
+export interface TabOptions {
+  transform?: (option: Option, node: PageTree.Folder) => Option | null;
+}
+
+export function getSidebarTabs(
+  pageTree: PageTree.Root,
+  { transform }: TabOptions = {},
+): Option[] {
+  const options: Option[] = [];
+
+  function traverse(node: PageTree.Node) {
+    if (node.type === 'folder' && node.root) {
+      const index = node.index ?? node.children.at(0);
+
+      if (index?.type === 'page') {
+        const option: Option = {
+          url: index.url,
+          title: node.name,
+          icon: node.icon,
+          folder: node,
+          description: node.description,
+        };
+
+        const mapped = transform ? transform(option, node) : option;
+        if (mapped) options.push(mapped);
+      }
+    }
+
+    if (node.type === 'folder') node.children.forEach(traverse);
+  }
+
+  pageTree.children.forEach(traverse);
+  return options;
+}
